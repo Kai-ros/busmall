70d45eb84f80a6e3f037de50c0a1c00c4bafa1f7
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-06-18 00:40:08 -0700

    Bedtime commit.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,13 @@
 'use strict';
 
-
-// image objects(constructor)
-// inline elem's CSS
-
-
-// receive clicks
-// track clicks per image 
-// track clicks in total 
-// track timesShown
-
-
-
 // upon click generate 3 new random images
 // displayed images - no duplicates of the 3 shown or the 3 previously shown
 
 
-
-// constructor function 
 // object properties: imageName, filepath, numberTimesShown, numberTimesClicked, htmlID
-
-
 
 // counter for 25 clicks
 // 25 < turn off listeners on images, display list of products and results, 
-
-
 
 // Begin Code
 
@@ -40,9 +22,9 @@ var rightTriptychImageTag = document.getElementById('right_triptych_image_tag');
 var totalClicks = 0;
 
 // Variables to store the initial images shown
-var leftInitialImage = null;
-var centerInitialImage = null;
-var rightInitialImage = null;
+var leftImageDisplayed = null;
+var centerImageDisplayed = null;
+var rightImageDisplayed = null;
 
 // Constructor
 var TriptychImage = function (name, imageSrc) {
@@ -55,29 +37,29 @@ var TriptychImage = function (name, imageSrc) {
   TriptychImage.allImages.push(this);
 };
 
-// Fill array with instantiated images
-TriptychImage.allImages = [
-  new TriptychImage('bag', '../img/bag.jpg'),
-  new TriptychImage('banana', '../img/banana.jpg'),
-  new TriptychImage('bathroom', '../img/bathroom.jpg'),
-  new TriptychImage('boots', '../img/boots.jpg'),
-  new TriptychImage('breakfast', '../img/breakfast.jpg'),
-  new TriptychImage('bubblegum', '../img/bubblegum.jpg'),
-  new TriptychImage('chair', '../img/chair.jpg'),
-  new TriptychImage('cthulhu', '../img/cthulhu.jpg'),
-  new TriptychImage('dog', '../img/dog.jpg'),
-  new TriptychImage('dragon', '../img/dragon.jpg'),
-  new TriptychImage('pen', '../img/pen.jpg'),
-  new TriptychImage('pet-sweep', '../img/pet-sweep.jpg'),
-  new TriptychImage('scissors', '../img/scissors.jpg'),
-  new TriptychImage('shark', '../img/shark.jpg'),
-  new TriptychImage('sweep', '../img/sweep.png'),
-  new TriptychImage('tauntaun', '../img/tauntaun.jpg'),
-  new TriptychImage('unicorn', '../img/unicorn.jpg'),
-  new TriptychImage('usb', '../img/usb.gif'),
-  new TriptychImage('water-can', '../img/water-can.jpg'),
-  new TriptychImage('wine-glass', '../img/wine-glass.jpg'),
-];
+// Instantiate array and fill it with goodies.
+TriptychImage.allImages = [];
+
+new TriptychImage('bag', '../img/bag.jpg');
+new TriptychImage('banana', '../img/banana.jpg');
+new TriptychImage('bathroom', '../img/bathroom.jpg');
+new TriptychImage('boots', '../img/boots.jpg');
+new TriptychImage('breakfast', '../img/breakfast.jpg');
+new TriptychImage('bubblegum', '../img/bubblegum.jpg');
+new TriptychImage('chair', '../img/chair.jpg');
+new TriptychImage('cthulhu', '../img/cthulhu.jpg');
+new TriptychImage('dog', '../img/dog.jpg');
+new TriptychImage('dragon', '../img/dragon.jpg');
+new TriptychImage('pen', '../img/pen.jpg');
+new TriptychImage('pet-sweep', '../img/pet-sweep.jpg');
+new TriptychImage('scissors', '../img/scissors.jpg');
+new TriptychImage('shark', '../img/shark.jpg');
+new TriptychImage('sweep', '../img/sweep.png');
+new TriptychImage('tauntaun', '../img/tauntaun.jpg');
+new TriptychImage('unicorn', '../img/unicorn.jpg');
+new TriptychImage('usb', '../img/usb.gif');
+new TriptychImage('water-can', '../img/water-can.jpg');
+new TriptychImage('wine-glass', '../img/wine-glass.jpg');
 
 var renderNewImages = function (leftIndex, centerIndex, rightIndex) {
   leftTriptychImageTag.src = TriptychImage.allImages[leftIndex].url;
@@ -85,72 +67,76 @@ var renderNewImages = function (leftIndex, centerIndex, rightIndex) {
   rightTriptychImageTag.src = TriptychImage.allImages[rightIndex].url;
 };
 
-var pickNewImages = function () {
-  var leftIndex = Math.round(Math.random() * TriptychImage.allImages.length - 1);
+// Is passed an array to create a copy of the actual array.
+var chooseRandomIndex = function (array) {
+  return Math.floor(Math.random() * array.length);
+};
 
-  do {
-    var rightIndex = Math.round(Math.random() * TriptychImage.allImages.length - 1);
-    var centerIndex = Math.round(Math.random() * TriptychImage.allImages.length - 1);
-  } while ((rightIndex === leftIndex) && (rightIndex === centerIndex) && (leftIndex === centerIndex));
+// Chooses 3 new unique images.
+var pickNewImages = function () {
+
+  var remainingIndices = [];
+
+  for (var i = 0; i < TriptychImage.allImages.length; i++) {
+    remainingIndices.push(i);
+  }
+
+  var leftIndex = remainingIndices.splice(chooseRandomIndex(remainingIndices), 1)[0];
+  var rightIndex = remainingIndices.splice(chooseRandomIndex(remainingIndices), 1)[0];
+  var centerIndex = remainingIndices.splice(chooseRandomIndex(remainingIndices), 1)[0];
+
 
   console.log(TriptychImage.allImages[leftIndex].name, TriptychImage.allImages[centerIndex].name, TriptychImage.allImages[rightIndex].name);
 
-  leftInitialImage = TriptychImage.allImages[leftIndex];
-  centerInitialImage = TriptychImage.allImages[centerIndex];
-  rightInitialImage = TriptychImage.allImages[rightIndex];
+  leftImageDisplayed = TriptychImage.allImages[leftIndex];
+  centerImageDisplayed = TriptychImage.allImages[centerIndex];
+  rightImageDisplayed = TriptychImage.allImages[rightIndex];
 
   renderNewImages(leftIndex, centerIndex, rightIndex);
 };
 
-var handleOnClickTriptych = function (event) {
-  console.log('im still alive');
-  // If they're still allowed to click, then perform clicky operations
-  if (totalClicks < 10) {
-
-    var imageClickedOn = event.target;
-    var id = imageClickedOn.id;
-
-    if ((id === 'left_triptych_image_tag') || (id === 'center_triptych_image_tag') || (id === 'right_triptych_image_tag')) {
-      // track the images
-      // increment the clicks of the appropriate slot
-      if (id === 'left_triptych_image_tag') {
-        leftInitialImage.clicks++;
-      }
-
-      if (id === 'center_triptych_image_tag') {
-        centerInitialImage.clicks++;
-      }
-
-      if (id === 'right_triptych_image_tag') {
-        rightInitialImage.clicks++;
-      }
-
-      // Increment all images number of times shown every event.
-      leftInitialImage.timesShown++;
-      centerInitialImage.timesShown++;
-      rightInitialImage.timesShown++;
-
-      //after we update the old, pick new pictures
-      pickNewImages();
-    }
-    console.log(event.target.id);
-  }
-
-  // Increment amount of clicks
-  totalClicks++;
-
-  // When they reach total max clicks, remove the clicky operations
-  if (totalClicks === 10) {
-    imageTriptychSectionTag.removeEventListener('click', handleOnClickTriptych);
-  }
+// Click handler section
+var trackImageClicks = function(image) {
+  image.clicks++;
+  image.timesShown++;
 };
 
-// Turn on click listener functionality.
-imageTriptychSectionTag.addEventListener('click', handleOnClickTriptych);
+var leftSelectButton = document.getElementById('left_image_button');
+leftSelectButton.addEventListener('click', function handler(event) {
+  if (totalClicks < 10) {
+    trackImageClicks(leftImageDisplayed);
+    totalClicks++;
+  } else {
+    event.target.removeEventListener('click', handler);
+  }
+});
 
-leftInitialImage = TriptychImage.allImages[3];
-centerInitialImage = TriptychImage.allImages[4];
-rightInitialImage = TriptychImage.allImages[0];
+var centerSelectButton = document.getElementById('center_image_button');
+centerSelectButton.addEventListener('click', function handler(event) {
+  if (totalClicks < 10) {
+    trackImageClicks(centerImageDisplayed);
+    totalClicks++;
+  } else {
+    event.target.removeEventListener('click', handler);
+  }
+});
+
+var rightSelectButton = document.getElementById('right_image_button');
+rightSelectButton.addEventListener('click', function handler(event) {
+  if (totalClicks < 10) {
+    trackImageClicks(rightImageDisplayed);
+    totalClicks++;
+  } else {
+    event.target.removeEventListener('click', handler);
+  }
+});
+
+// Turn on click listener functionality.
+imageTriptychSectionTag.addEventListener('click', trackImageClicks);
+
+leftImageDisplayed = TriptychImage.allImages[3];
+centerImageDisplayed = TriptychImage.allImages[4];
+rightImageDisplayed = TriptychImage.allImages[0];
 
 pickNewImages();
 
